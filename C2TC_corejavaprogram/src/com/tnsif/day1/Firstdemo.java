ea425d1e76f8c6d14f5f1cef6219c4aeaf7725b5
keert <keert@MSI>
2025-09-24 15:09:03 +0530

    This project is my example practices of core java

---
+++ C2TC_corejavaprogram/src/com/tnsif/day1/Firstdemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.day1;
+
+public class Firstdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Full stack java developer course");
+		System.out.println("Welcome to TNSIF training");
+		System.out.print("IFET college");
+         
+	}
+
+}
+
+
